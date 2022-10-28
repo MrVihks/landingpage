ebfa686120eb55e80e35d53055283070b8b1d752
Vini <viniciushkds@gmail.com>
2022-10-28 11:45:54 -0300

    primeiro commit

---
+++ src/componets/navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <>
+            <div>
+                <nav>
+                    <a>Inicio</a>
+                    <a>Produtos</a>
+                    <a>Sobre nós</a>
+                </nav>
+            </div>
+        </>
+    )
+}
